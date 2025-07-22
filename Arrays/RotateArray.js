80f301baf499b2f3d28337f13690174669b9753d
Ash-jo121 <ashishkaloor@gmail.com>
2025-07-22 23:42:37 +0530

    Fix: Added section for Arrays

---
+++ Arrays/RotateArray.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  let n = nums.length;
+  let temp = [];
+  let startX = n - k;
+
+  for (let i = startX; i < n; i++) {
+    temp.push(nums[i]);
+  }
+
+  for (let i = startX - 1; i >= 0; i--) {
+    nums[i + k] = nums[i];
+  }
+
+  for (let i = 0; i < temp.length; i++) {
+    nums[i] = temp[i];
+  }
+};
+
+rotate([-1, -100, 3, 99], 2);
